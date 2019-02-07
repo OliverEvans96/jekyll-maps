8de86fbeccd88109a2620d505d49273b860ae118
Oliver Evans <oliverevans96@gmail.com>
2019-02-07 17:05:43 -0500

    Fix filter checking logic
    Previously, if a matching src filter was found, the method would return
    before checking other filters. This caused all filters after "src" to be
    ignored.

--- lib/jekyll-maps/location_finder.rb
+++ lib/jekyll-maps/location_finder.rb
@@ -76,12 +76,16 @@ module Jekyll
       def match_filters?(doc)
         @options[:filters].each do |filter, value|
           if filter == "src"
-            return true unless doc.respond_to?(:relative_path)
-            return false unless doc.relative_path.start_with?(value)
+            if doc.respond_to?(:relative_path)
+              if doc.relative_path.start_with?(value)
+                return false
+              end
+            end
           elsif doc[filter].nil? || doc[filter] != value
             return false
           end
         end
+        return true
       end
 
       private
